7e65ea38479bc695ee126ef3d757024104d291a5
clairemcwhite <claire.mcwhite@utexas.edu>
2017-08-07 20:59:00 -0500

    Revert "Move optional arguments to end of function args"
    This reverts commit d56b0553ef8f767fff9f080910b75b7b4097007d.
    
    Conflicts:
    	R/cov_columns.R
    	R/covplot.R

--- R/cov_columns.R
+++ R/cov_columns.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #' Append columns related to the coverage of a protein.
 #'
 #' @param data_table Data frame to which the coverage columns will be appended.
@@ -85,3 +86,89 @@ cov_columns <- function(data_table,proteome,groupid,elementid,export=FALSE, outn
         'Your file has been saved as' %>% paste(outname) %>% cat
     }
 }
+=======
+#' Append columns related to the coverage of a protein.
+#'
+#' @param data_table Data frame to which the coverage columns will be appended.
+#' @param proteome FASTA file from which the sequences will be extracted.
+#' @param outname If export = TRUE, output the resulting .
+#' @param groupid Data frame column (factor) corresponding to the protein name to be matched with the IDs extracted from the FASTA file.
+#' @param elementid Data frame column (factor) corresponding to the peptide sequence to be searched against the sequences extracted from the FASTA file.
+#' @param export If false (default), return a data frame. If true, write a .csv file named after the input file + '_cov.csv'.
+#' @import dplyr
+#' @import stringr
+#' @import purrr
+#' @import lazyeval
+#' @import seqinr
+#' @return Data frame with the following added columns: "Start", "End", "Sequence", "Appearance".
+#' @export
+#' @examples
+#' cov_columns(test_data,proteome='sequences.fasta',groupid='ID',elementid='Peptide')
+
+# suppressPackageStartupMessages(library(tidyverse))
+# suppressPackageStartupMessages(library(lazyeval))
+# suppressPackageStartupMessages(library(seqinr))
+
+
+cov_columns <- function(data_table,proteome,outname=paste0(deparse(substitute(data_table)),'_cov.csv'),groupid,elementid,export=FALSE){
+    fasta <- read.fasta(proteome)
+
+    list_seq <- lapply(fasta,function(lst){lst[1:length(lst)] %>% paste(collapse='') %>% toupper()})
+    list_seq <- data.frame(names(list_seq) %>% as_vector(),unname(list_seq) %>% as_vector())
+    names(list_seq) <- c(groupid,'Sequence')
+
+    tmp <- merge(x = data_table, y = list_seq, by = groupid, all.x = TRUE)
+
+
+    pep <- tmp %>% mutate_(.dots = setNames(list(interp(~gsub(pattern='I',replacement='J',x=var),var=as_name(elementid))),elementid))
+    pep <- pep %>% mutate_(.dots = setNames(list(interp(~gsub(pattern='L',replacement='J',x=var),var=as_name(elementid))),elementid))
+    pep <- pep %>%  mutate_(.dots = setNames(list(interp(~gsub(pattern='J',replacement='(I|L)',x=var),var=as_name(elementid))),elementid))
+    peptide_column <- pep[,colnames(pep)==elementid]
+    sequence_column <- pep[,colnames(pep)=='Sequence'] %>% as.character()
+
+    Length <- nchar(sequence_column)
+
+    Start <- rep('',length(peptide_column))
+    for(i in 1:length(peptide_column)){
+        if(length(str_locate_all(sequence_column[i],peptide_column[i])[[1]][,1]) == 0){
+            Start[i] <- 'Not found'
+        }else if(length(str_locate_all(sequence_column[i],peptide_column[i])[[1]][,1]) == 1){
+            Start[i] <- str_locate_all(sequence_column[i],peptide_column[i])[[1]][,1]
+        }else{
+            Start[i] <- str_locate_all(sequence_column[i],peptide_column[i])[[1]][,1] %>% list()
+        }
+    }
+
+    Start <- as.integer(Start)
+
+    End <- rep('',length(peptide_column))
+    for(i in 1:length(peptide_column)){
+        if(length(str_locate_all(sequence_column[i],peptide_column[i])[[1]][,2]) == 0){
+            End[i] <- 'Not found'
+        }else if(length(str_locate_all(sequence_column[i],peptide_column[i])[[1]][,2]) == 1){
+            End[i] <- str_locate_all(sequence_column[i],peptide_column[i])[[1]][,2]
+        }else{
+            End[i] <- str_locate_all(sequence_column[i],peptide_column[i])[[1]][,2] %>% list()
+        }}
+
+    End <- as.integer(End)
+
+
+    Appearance <- rep('',length(peptide_column))
+    for(i in 1:length(peptide_column)){
+        Appearance[i] <- length(str_locate_all(sequence_column[i],peptide_column[i])[[1]][,1])
+    }
+
+    Appearance <- as.integer(Appearance)
+
+    cov_cols <- data.frame(Length,Start,End,Appearance)
+
+    out_table <- cbind(data_table,cov_cols)
+    if(export==FALSE){
+        return(out_table)
+    }else{
+        write.csv(out_table,outname)
+        'Your file has been saved as' %>% paste(outname) %>% cat
+    }
+}
+>>>>>>> parent of d56b055... Move optional arguments to end of function args
